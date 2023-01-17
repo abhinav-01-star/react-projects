6c290981b2cfc9ffa8c3e1870457ff5a507d553e
abhinav-01-star <simhadriabhinav111@gmail.com>
2023-01-17 18:38:18 +0530

    project-3

---
+++ counter_app_using_hooks/src/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter+1);
+  }
+  return <div>
+  <p>{counter}</p>
+  <button onClick={increment}>Increment</button>
+  </div>;
+}
+
+const el = <Counter />; 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  el
+);
